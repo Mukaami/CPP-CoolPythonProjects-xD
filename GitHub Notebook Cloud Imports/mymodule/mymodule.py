7cf31df0a4680f69c878e0d6750863d085540adf
Mukaami <134884602+Mukaami@users.noreply.github.com>
2023-06-27 12:55:18 +0500

    Create mymodule.py

---
+++ GitHub Notebook Cloud Imports/mymodule/mymodule.py
@@ -0,0 +1,13 @@
+# mymodule.py
+
+def greet(name):
+    """
+    A dummy function that greets the given name.
+    
+    Args:
+        name (str): The name to greet.
+    
+    Returns:
+        str: A greeting message.
+    """
+    return f"Hello, {name}! Welcome to my module."
